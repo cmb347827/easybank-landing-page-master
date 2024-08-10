2d7ea8084c6dd4053a946559e2d46a9f58f924ee
cmb347827 <v38d422ll621dem@gmail.com>
2024-08-10 00:47:40 -0400

    initial commit

---
+++ easybank-landing-page-master.js
@@ -0,0 +1,22 @@
+'use strict'; 
+
+$(window).resize(function(){
+	location.reload();
+});
+
+
+$(window).on('load',function(){
+    
+	$("#navbarCollapse").on("show.bs.collapse", function(){
+        //menu is collapsed, show close icon.
+		$('#open').toggleClass('hidden');
+		$('#close').toggleClass('show');
+		$('#nav').addClass('custom-nav');
+    });
+	$("#navbarCollapse").on("hide.bs.collapse", function(){
+        $('#close').toggleClass('show');
+		$('#open').toggleClass('hidden');
+		$('#nav').removeClass('custom-nav');
+    });
+
+});
