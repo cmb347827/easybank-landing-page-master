4731f0946817345e34103029552bb398c17781ee
cmb347827 <v38d422ll621dem@gmail.com>
2024-09-12 23:40:33 -0400

    Aliging items further

--- easybank-landing-page-master.js
+++ easybank-landing-page-master.js
@@ -9,7 +9,7 @@ $(window).on('load',function(){
     
 	$("#navbarCollapse").on("show.bs.collapse", function(){
         //menu is collapsed, show close icon.
-		$('#open').toggleClass('hidden');
+		$('#open').toggleClass('hidden').attr('aria-hidden','true');
 		$('#close').toggleClass('show').attr('aria-hidden',"false");
 		$('.toggle').attr('aria-expanded',"true");
 		$('.outer').addClass('overlay');
@@ -17,7 +17,7 @@ $(window).on('load',function(){
     });
 	$("#navbarCollapse").on("hide.bs.collapse", function(){
         $('#close').toggleClass('show').attr('aria-hidden',"true");
-		$('#open').toggleClass('hidden');
+		$('#open').toggleClass('hidden').attr('aria-hidden','false');
 		$('.toggle').attr('aria-expanded',"false");
 		$('.outer').removeClass('overlay');
     });
